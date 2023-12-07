bce975b6abfad0f401b01120c370df704672444f
TaK0JR <153171725+TaK0JR@users.noreply.github.com>
2023-12-07 14:40:02 +0100

    init

---
+++ wp-content/themes/WordPress-theme-boilerplate-master/header.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+
+<html lang="en" dir="ltr">
+
+<head>
+    <meta charset="utf-8">
+
+    <meta name="viewport" content="user-scalable=no, initial-scale=1, maximum-scale=1, minimum-scale=1, width=device-width, height=device-height" />
+
+    <link rel="stylesheet" href="<?php bloginfo('template_url'); ?>/minireset.min.css">
+    <link rel="stylesheet" href="<?php bloginfo('template_url'); ?>/style.css">
+
+    <?php wp_head(); ?>
+
+</head>
+
+
+<body <?php body_class(); ?>>
