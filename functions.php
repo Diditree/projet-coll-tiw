8769c5ba016780498ea9a6ade45d782b93044a9a
Dimitri <dimitrilpl@gmail.com>
2023-12-07 14:51:00 +0100

    :fireworks: create functions.php
    création de functions.php

---
+++ functions.php
@@ -0,0 +1,9 @@
+<?php
+add_theme_support('menus');
+add_theme_support('title-tag');
+
+add_action('wp_enqueue_scripts', 'insert_css');
+
+function insert_css() {
+    wp_enqueue_style('style', get_stylesheet_uri());
+}
