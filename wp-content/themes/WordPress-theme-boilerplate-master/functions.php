ec701de3154cb0113258d5cb6108fcf5d05d265f
Dimitri <dimitrilpl@gmail.com>
2023-12-14 11:07:06 +0100

    :sparkles:
    functions

--- wp-content/themes/WordPress-theme-boilerplate-master/functions.php
+++ wp-content/themes/WordPress-theme-boilerplate-master/functions.php
@@ -1,12 +1,15 @@
 <?php
-
+add_theme_support('menus');
 add_theme_support('title-tag');
+add_action('wp_enqueue_scripts', 'insert_css');
 
-// ########## MENU ##########
+function insert_css()
+{
+    wp_enqueue_style('style', get_stylesheet_uri());
+}
 
-// add_theme_support( 'menus' );
-
-// register_nav_menus( [
-// 	'main-menu'  => 'Main navigation',
-// 	'footer-menu'     => 'Footer navigation'
-// ] );
+register_nav_menus([
+    'menu-principal' => 'Navigation principale',
+    'socialnetworks' => 'Réseaux sociaux',
+    'footerlegal' => 'Informations légales (footer)'
+]);
