e9683b319c63516c401dea703e22b10c51a1c15a
Dimitri <dimitrilpl@gmail.com>
2023-12-14 10:01:30 +0100

    :sparkles: menu wordpress
    création de la nav dans functions.php

--- functions.php
+++ functions.php
@@ -1,9 +1,15 @@
 <?php
 add_theme_support('menus');
 add_theme_support('title-tag');
-
 add_action('wp_enqueue_scripts', 'insert_css');
 
-function insert_css() {
+function insert_css()
+{
     wp_enqueue_style('style', get_stylesheet_uri());
 }
+
+register_nav_menus([
+    'menu-principal' => 'Navigation principale',
+    'socialnetworks' => 'Réseaux sociaux',
+    'footerlegal' => 'Informations légales (footer)'
+]);
