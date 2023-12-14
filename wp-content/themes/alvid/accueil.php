65178ee220e7f84e397a37fc7c2a471917c55920
evelem <153066873+evelem@users.noreply.github.com>
2023-12-14 16:10:25 +0100

    Mise à jour champs ACF
    Mise à jour des pages contact et galerie + création du formulaire de contact.

---
+++ wp-content/themes/alvid/accueil.php
@@ -0,0 +1,3 @@
+<?php /* Template Name: accueil */ ?>
+<?php get_header() ?>
+<?php get_footer() ?>
---
+++ wp-content/themes/alvid/contact.php
@@ -0,0 +1,3 @@
+<?php /* Template Name: contact */ ?>
+<?php get_header() ?>
+<?php get_footer() ?>
