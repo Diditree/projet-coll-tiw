10f164be913da96018ee18e12c7b97dd22012536
Dimitri <dimitrilpl@gmail.com>
2024-01-18 12:22:05 +0100

    Update front-page.php

--- wp-content/themes/alvid/front-page.php
+++ wp-content/themes/alvid/front-page.php
@@ -1,6 +1,27 @@
 <?php get_header() ?>
+<<<<<<< Updated upstream
 
 <header></header>
+=======
+<header>
+    <section class="container">
+        <div class="row">
+            <div class="col-auto col-md-4">
+                <img src="https://placewaifu.com/image/200">
+            </div>
+            <div class="col-auto col-md-8">
+                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eveniet maxime possimus illo voluptates,
+                    ipsa voluptas omnis qui facere earum quae cum, totam fugit aliquam soluta odio asperiores rem vel
+                    voluptatum!</p>
+            </div>
+        </div>
+    </section>
+</header>
+<main>
+
+    <section class="container">
+        <h2 class="text-center">Lorem ipsum dolor sit amet.</h2>
+>>>>>>> Stashed changes
 
 <main></main>
 
